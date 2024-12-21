3c05d5607b18ffb9e9fb00aa3a2d4e56edf199df
Jivan-lvm <serezhkin.ivan@gmail.com>
2024-12-22 02:29:51 +0700

    add register

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import Fastapi
 import uvicorn
+from app.users.router import router as router_users
 
 
 app = Fastapi()
+
+app.include_router(router_users)
 
 
 if __name__ == '__main__':
